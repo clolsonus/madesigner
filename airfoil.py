7ccee6d8858c43a3354a7902e469210b963c1373
Curtis L. Olson <curt0001@flightgear.org>
2012-12-21 21:07:40 -0600

    Spline interpolation and resampling the airfoil at a different step size now works!

--- airfoil.py
+++ airfoil.py
@@ -8,6 +8,7 @@ __license__ = "GPL v2"
 
 import fileinput
 import string
+import spline
 
 datapath = "./data"
 
@@ -42,6 +43,59 @@ class Airfoil:
         self.top.reverse()
         print self.description + " (" + self.name + ") Loaded " + str(len(self.top) + len(self.bottom)) + " points"
 
+    def simple_interp(self, points, v):
+        index = spline.binsearch(points, v)
+        n = len(points) - 1
+        if index < n:
+            xrange = points[index+1][0] - points[index][0]
+            yrange = points[index+1][1] - points[index][1]
+	    # print(" xrange = $xrange\n");
+            percent = (v - points[index][0]) / xrange
+	    # print(" percent = $percent\n");
+            return points[index][1] + percent * yrange
+        else:
+            return points[index][1]
+
+    def resample(self, xdivs, use_spline):
+        result = Airfoil()
+        result.name = self.name
+        result.description = self.description
+        step = 1.0 / xdivs
+        top_y2 = spline.derivative2( self.top )
+        bottom_y2 = spline.derivative2( self.bottom )
+        for i in range(0, xdivs+1):
+            x = i * step
+            if use_spline:
+                index = spline.binsearch(self.top, x)
+                y = spline.spline(self.top, top_y2, index, x)
+                #print str(index) + " " + str(y)
+            else:
+                y = self.simple_interp(self.top, x )
+            result.top.append( (x, y) )
+
+        for i in range(0, xdivs+1):
+            x = i * step
+            if use_spline:
+                index = spline.binsearch(self.bottom, x)
+                y = spline.spline(self.bottom, bottom_y2, index, x)
+            else:
+                y = self.simple_interp(self.bottom, x )
+            result.bottom.append( (x, y) )
+        return result
+
+    def display(self):
+        for pt in self.top:
+            print str(pt[0]) + " " + str(pt[1])
+        for pt in self.bottom:
+            print str(pt[0]) + " " + str(pt[1])
+
 
 root = Airfoil("clarky");
 tip = Airfoil("arad6");
+#root.display()
+#tip.display()
+
+root_smooth = root.resample(1000, True)
+tip_smooth = tip.resample(1000, False)
+root_smooth.display()
+tip_smooth.display()
