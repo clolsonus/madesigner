b782525e4c2f5a65eb2a985c5b5c400beb22a260
Curtis L. Olson <curt0001@flightgear.org>
2012-12-27 12:24:23 -0600

    Make airfoil methods self modifying (rather than returning a new airfoil.) This makes the top level code more naturual and makes things like stringer cutouts easier to manage.
    First working stringer cutout method.  Cutouts can be tangent to the slope
    or forced vertical.

--- airfoil.py
+++ airfoil.py
@@ -15,15 +15,17 @@ datapath = "./data"
 
 class Airfoil:
 
-    def __init__(self, name = ""):
+    def __init__(self, name = "", samples = 0, use_spline = False):
         self.name = ""
         self.description = ""
+        self.raw_top = []
+        self.raw_bottom = []
         self.top = []
         self.bottom = []
         if ( name != "" ):
-            self.load(name)
+            self.load(name, samples, use_spline)
 
-    def load(self, base):
+    def load(self, base, samples = 0, use_spline = False):
         self.name = base
         path = datapath + "/airfoils/" + base + ".dat"
         top = True
@@ -35,14 +37,19 @@ class Airfoil:
                 x = float(xa)
                 y = float(ya)
                 if top:
-                    self.top.append( (x,y) )
+                    self.raw_top.append( (x,y) )
                 if x < 0.000001:
                     top = not top
                 if not top:
-                    self.bottom.append( (x,y) )
+                    self.raw_bottom.append( (x,y) )
                 # print "x = " + str(x) + " y = " + str(y)
-        self.top.reverse()
+        self.raw_top.reverse()
         # print self.description + " (" + self.name + ") Loaded " + str(len(self.top) + len(self.bottom)) + " points"
+        if samples > 0:
+            self.resample( samples, use_spline )
+        else:
+            self.top = list(self.raw_top)
+            self.bottom = list(self.raw_bottom)
 
     def simple_interp(self, points, v):
         index = spline.binsearch(points, v)
@@ -58,31 +65,29 @@ class Airfoil:
             return points[index][1]
 
     def resample(self, xdivs, use_spline):
-        result = Airfoil()
-        result.name = self.name
-        result.description = self.description
+        self.top = []
+        self.bottom = []
         step = 1.0 / xdivs
-        top_y2 = spline.derivative2( self.top )
-        bottom_y2 = spline.derivative2( self.bottom )
+        top_y2 = spline.derivative2( self.raw_top )
+        bottom_y2 = spline.derivative2( self.raw_bottom )
         for i in range(0, xdivs+1):
             x = i * step
             if use_spline:
-                index = spline.binsearch(self.top, x)
-                y = spline.spline(self.top, top_y2, index, x)
+                index = spline.binsearch(self.raw_top, x)
+                y = spline.spline(self.raw_top, top_y2, index, x)
                 #print str(index) + " " + str(y)
             else:
-                y = self.simple_interp(self.top, x )
-            result.top.append( (x, y) )
+                y = self.simple_interp(self.raw_top, x )
+            self.top.append( (x, y) )
 
         for i in range(0, xdivs+1):
             x = i * step
             if use_spline:
-                index = spline.binsearch(self.bottom, x)
-                y = spline.spline(self.bottom, bottom_y2, index, x)
+                index = spline.binsearch(self.raw_bottom, x)
+                y = spline.spline(self.raw_bottom, bottom_y2, index, x)
             else:
-                y = self.simple_interp(self.bottom, x )
-            result.bottom.append( (x, y) )
-        return result
+                y = self.simple_interp(self.raw_bottom, x )
+            self.bottom.append( (x, y) )
 
     def display(self):
         for pt in self.top:
@@ -90,109 +95,127 @@ class Airfoil:
         for pt in self.bottom:
             print str(pt[0]) + " " + str(pt[1])
 
-    def rotate(self, angle):
-        result = Airfoil()
-        result.name = self.name
-        result.description = self.description + " rotated by " + str(angle) + " degrees"
+    def rotate_point( self, pt, angle ):
         rad = math.radians(angle)
+        newx = pt[0] * math.cos(rad) - pt[1] * math.sin(rad)
+        newy = pt[1] * math.cos(rad) + pt[0] * math.sin(rad)
+        return (newx, newy)
+
+    def rotate(self, angle):
+        newtop = []
+        newbottom = []
         for pt in self.top:
-            newx = pt[0] * math.cos(rad) - pt[1] * math.sin(rad)
-            newy = pt[1] * math.cos(rad) + pt[0] * math.sin(rad)
-            result.top.append( (newx, newy) )
+            newtop.append( self.rotate_point(pt, angle) )
         for pt in self.bottom:
-            newx = pt[0] * math.cos(rad) - pt[1] * math.sin(rad)
-            newy = pt[1] * math.cos(rad) + pt[0] * math.sin(rad)
-            result.bottom.append( (newx, newy) )
-        return result
+            newbottom.append( self.rotate_point(pt, angle) )
+        self.top = list(newtop)
+        self.bottom = list(newbottom)
 
     def scale(self, hsize, vsize):
-        result = Airfoil()
-        result.name = self.name
-        result.description = self.description + " scaled by " + str(hsize) + "," + str(vsize)
+        newtop = []
+        newbottom = []
         for pt in self.top:
             newx = pt[0] * hsize
             newy = pt[1] * vsize
-            result.top.append( (newx, newy) )
+            newtop.append( (newx, newy) )
         for pt in self.bottom:
             newx = pt[0] * hsize
             newy = pt[1] * vsize
-            result.bottom.append( (newx, newy) )
-        return result
+            newbottom.append( (newx, newy) )
+        self.top = list(newtop)
+        self.bottom = list(newbottom)
 
     def move(self, x, y):
-        result = Airfoil()
-        result.name = self.name
-        result.description = self.description + " moved by " + str(x) + "," + str(y)
+        newtop = []
+        newbottom = []
         for pt in self.top:
             newx = pt[0] + x
             newy = pt[1] + y
-            result.top.append( (newx, newy) )
+            newtop.append( (newx, newy) )
         for pt in self.bottom:
             newx = pt[0] + x
             newy = pt[1] + y
-            result.bottom.append( (newx, newy) )
-        return result
+            newbottom.append( (newx, newy) )
+        self.top = list(newtop)
+        self.bottom = list(newbottom)
 
-    # rel top/bottom, abs x,y, tangent y/n
-    def cutout_rel(self, top, xpos, xsize, ysize):
-        result = Airfoil()
-        result.name = self.name
-        result.description = self.description
+    # rel top/bottom, abs x,y, tangent y/n, wrap y/n
+    def cutout_stringer(self, side = "top", orientation = "tangent", xpos = 0, xsize = 0, ysize = 0):
         xhalf = xsize / 2
-        xstart = xpos - xhalf
-        xend = xpos + xhalf
-        # top segment
-        if top:
-            n = len(self.top)
-            ytop = self.simple_interp(self.top, xpos)
-            i = 0
-            # nose portion
-            while self.top[i][0] < xstart and i < n:
-                result.top.append( self.top[i] )
-                i += 1
-            # cut out
-            ystart = self.simple_interp(self.top, xstart)
-            yend = self.simple_interp(self.top, xstart)
-            result.top.append( (xstart, ystart) )
-            result.top.append( (xstart, ytop - ysize) )
-            result.top.append( (xend, ytop - ysize) )
-            result.top.append( (xend, yend) )
-            # skip airfoil coutout points
-            while self.top[i][0] <= xend and i < n:
-                i += 1
-            # tail portion
-            while i < n:
-                result.top.append( self.top[i] )
-                i += 1
-        else:
-            result.top = self.top
-        # bottom segment
-        if top:
-            result.bottom = self.bottom
-        else:
-            n = len(self.bottom)
-            ybottom = self.simple_interp(self.bottom, xpos)
-            i = 0
-            # nose portion
-            while self.bottom[i][0] < xstart and i < n:
-                result.bottom.append( self.bottom[i] )
-                i += 1
-            # cut out
-            ystart = self.simple_interp(self.bottom, xstart)
-            yend = self.simple_interp(self.bottom, xstart)
-            result.bottom.append( (xstart, ystart) )
-            result.bottom.append( (xstart, ybottom + ysize) )
-            result.bottom.append( (xend, ybottom + ysize) )
-            result.bottom.append( (xend, yend) )
-            # skip airfoil coutout points
-            while self.bottom[i][0] <= xend and i < n:
-                i += 1
-            # tail portion
-            while i < n:
-                result.bottom.append( self.bottom[i] )
-                i += 1
 
-        return result
+        top = False
+        if side == "top":
+            top = True
+        tangent = False
+        if orientation == "tangent":
+            tangent = True;
+
+        curve = []
+        if top:
+            curve = list(self.top)
+        else:
+            curve = list(self.bottom)
+
+        n = len(curve)
+        slopes = spline.derivative1(curve)
+
+        newcurve = []
+        ypos = self.simple_interp(curve, xpos)
+
+        angle = 0
+        if tangent:
+            index = spline.binsearch(curve, xpos)
+            slope = slopes[index]
+            rad = math.atan2(slope,1)
+            angle = math.degrees(rad)
+        if not top:
+            angle += 180
+            if angle > 360:
+                angle -= 360
+        r0 = self.rotate_point( (-xhalf, 0), angle )
+        r1 = self.rotate_point( (-xhalf, -ysize), angle )
+        r2 = self.rotate_point( (xhalf, -ysize), angle )
+        r3 = self.rotate_point( (xhalf, 0), angle )
+        if tangent:
+            p0 = ( r0[0] + xpos, r0[1] + ypos )
+            p1 = ( r1[0] + xpos, r1[1] + ypos )
+            p2 = ( r2[0] + xpos, r2[1] + ypos )
+            p3 = ( r3[0] + xpos, r3[1] + ypos )
+        else:
+            x = r0[0] + xpos
+            p0 = ( r0[0] + xpos, self.simple_interp(curve, x) )
+            p1 = ( r1[0] + xpos, r1[1] + ypos )
+            p2 = ( r2[0] + xpos, r2[1] + ypos )
+            x = r3[0] + xpos
+            p3 = ( r3[0] + xpos, self.simple_interp(curve, x) )
+
+        i = 0
+        # nose portion
+        while (curve[i][0] < p0[0] and curve[i][0] < p3[0]) and i < n:
+            newcurve.append( curve[i] )
+            i += 1
+        # cut out
+        if top:
+            newcurve.append( p0 )
+            newcurve.append( p1 )
+            newcurve.append( p2 )
+            newcurve.append( p3 )
+        else:
+            newcurve.append( p3 )
+            newcurve.append( p2 )
+            newcurve.append( p1 )
+            newcurve.append( p0 )
+        # skip airfoil coutout points
+        while (curve[i][0] <= p0[0] or curve[i][0] <= p3[0]) and i < n:
+            i += 1
+        # tail portion
+        while i < n:
+            newcurve.append( curve[i] )
+            i += 1
+        if top:
+            self.top = list(newcurve)
+        else:
+            self.bottom = list(newcurve)
 
 def blend( af1, af2, percent ):
     result = Airfoil()
@@ -211,6 +234,8 @@ def blend( af1, af2, percent ):
 	y2 = af2.bottom[i][1]
 	y = percent*y1 + (1.0-percent)*y2
 	result.bottom.append( (af1.bottom[i][0], y) )
+    result.raw_top = list(result.top)
+    result.raw_bottom = list(result.bottom)
     return result
 
 #root = Airfoil("clarky")
