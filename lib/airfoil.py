382d593e6ac8166f70a4cdad3ea08e9ca59cfaf1
Curtis L. Olson <curt0001@flightgear.org>
2013-01-01 17:10:05 -0600

    Add support of labels and holes in the airfoil class Add a fledgling layout class to begin to attack the challenges of laying out the parts onto sheets in some way that isn't completely wasteful.

--- lib/airfoil.py
+++ lib/airfoil.py
@@ -25,6 +25,7 @@ class Airfoil:
         self.top = []
         self.bottom = []
         self.holes = []
+        self.labels = []
         self.nosedist = 0.0
         if ( name != "" ):
             self.load(name, samples, use_spline)
@@ -190,17 +191,28 @@ class Airfoil:
     def rotate(self, angle):
         newtop = []
         newbottom = []
+        newholes = []
+        newlabels = []
         for pt in self.top:
             newtop.append( self.rotate_point(pt, angle) )
         for pt in self.bottom:
             newbottom.append( self.rotate_point(pt, angle) )
+        for hole in self.holes:
+            newpt = self.rotate_point( (hole[0], hole[1]), angle)
+            newholes.append( (newpt[0], newpt[1], hole[2]) )
+        for label in self.labels:
+            newpt = self.rotate_point( (label[0], label[1]), angle)
+            newlabels.append( (newpt[0], newpt[1], label[2], label[3] + angle, label[4]) )
         self.top = list(newtop)
         self.bottom = list(newbottom)
+        self.holes = list(newholes)
+        self.labels = list(newlabels)
 
     def scale(self, hsize, vsize):
         newtop = []
         newbottom = []
         newholes = []
+        newlabels = []
         for pt in self.top:
             newx = pt[0] * hsize
             newy = pt[1] * vsize
@@ -217,13 +229,20 @@ class Airfoil:
             else:
                 newr = ( hole[2] * vsize )
             newholes.append( (newx, newy, newr) )
+        for label in self.labels:
+            newx = ( label[0] * hsize )
+            newy = ( label[1] * vsize )
+            newlabels.append( (newx, newy, label[2], label[3], label[4]) )
         self.top = list(newtop)
         self.bottom = list(newbottom)
         self.holes = list(newholes)
+        self.labels = list(newlabels)
 
     def move(self, x, y):
         newtop = []
         newbottom = []
+        newholes = []
+        newlabels = []
         for pt in self.top:
             newx = pt[0] + x
             newy = pt[1] + y
@@ -232,8 +251,18 @@ class Airfoil:
             newx = pt[0] + x
             newy = pt[1] + y
             newbottom.append( (newx, newy) )
+        for hole in self.holes:
+            newx = hole[0] + x
+            newy = hole[1] + y
+            newholes.append( (newx, newy, hole[2]) )
+        for label in self.labels:
+            newx = label[0] + x
+            newy = label[1] + y
+            newlabels.append( (newx, newy, label[2], label[3], label[4]) )
         self.top = list(newtop)
         self.bottom = list(newbottom)
+        self.holes = list(newholes)
+        self.labels = list(newlabels)
 
     def walk_curve_dist(self, curve, xstart, target_dist):
         #print "walk from " + str(xstart) + " dist of " + str(target_dist)
@@ -425,6 +454,9 @@ class Airfoil:
 
     def add_hole(self, xpos, ypos, radius):
         self.holes.append( (xpos, ypos, radius) )        
+
+    def add_label(self, xpos, ypos, size, rotate, text):
+        self.labels.append( (xpos, ypos, size, rotate, text) )        
 
     def project_point(self, top, slopes, index, orig, ysize):
         slope = slopes[index]
---
+++ lib/layout.py
@@ -0,0 +1,82 @@
+#!python
+
+__author__ = "Curtis L. Olson < curtolson {at} flightgear {dot} org >"
+__url__ = "http://gallinazo.flightgear.org"
+__version__ = "1.0"
+__license__ = "GPL v2"
+
+
+import copy
+
+import svgwrite
+
+import airfoil
+
+
+class Layout:
+
+    def __init__(self, name, width_in, height_in, dpi):
+        self.dwg = svgwrite.Drawing( name, size = ( '{:.2f}in'.format(width_in), '{:.2f}in'.format(height_in) ) )
+        self.width_in = width_in
+        self.height_in = height_in
+        self.dpi = dpi
+
+    def draw_airfoil(self, orig_airfoil, xpos, ypos, stroke_width, color, lines = True, points = False ):
+        airfoil = copy.deepcopy(orig_airfoil)
+        airfoil.scale(1,-1)
+        bounds = airfoil.get_bounds()
+        dx = bounds[1][0] - bounds[0][0]
+        dy = bounds[1][1] - bounds[0][1]
+
+        marginx = (self.width_in - dx) * 0.5
+        marginy = (self.height_in - dy) * 0.5
+    
+        airfoil.scale( self.dpi, self.dpi )
+        reverse_top = list(airfoil.top)
+        reverse_top.reverse()
+        shape = reverse_top + airfoil.bottom
+        g = self.dwg.g()
+        g.translate((marginx-bounds[0][0])*self.dpi,(ypos-bounds[0][1])*self.dpi)
+        if lines:
+            poly = self.dwg.polygon(shape, stroke = 'red', fill = 'none', \
+                                        stroke_width = stroke_width)
+            g.add( poly )
+
+        for hole in airfoil.holes:
+            pt = ( hole[0], hole[1] )
+            radius = hole[2]
+            c = self.dwg.circle( center = pt, r = radius, stroke = 'red', \
+                                     fill = 'none', \
+                                     stroke_width = '{:.4f}in'.format(stroke_width_in))
+            g.add(c)
+
+        for label in airfoil.labels:
+            #print "label = " + str(label[0]) + "," + str(label[1])
+            t = self.dwg.text(label[4], (0, 0), font_size = label[2], text_anchor = "middle")
+            t.translate( (label[0], label[1]) )
+            t.rotate(-label[3])
+            # text_align = center
+            g.add(t)
+
+        if points:
+            for pt in shape:
+                c = self.dwg.circle( center = pt, r = 2, stroke = 'green', \
+                                    fill = 'green', opacity = 0.6)
+                g.add(c)
+
+        self.dwg.add(g)
+
+    def draw_airfoil_cut(self, airfoil, xpos, ypos ):
+        self.draw_airfoil(airfoil, xpos, ypos, '0.001in', 'red', True, False )
+
+    def draw_airfoil_plan(self, airfoil, xpos, ypos ):
+        self.draw_airfoil(airfoil, xpos, ypos, '1px', 'red', True, False )
+
+    def draw_airfoil_demo(self, airfoil, xpos, ypos ):
+        self.draw_airfoil(airfoil, xpos, ypos, '1px', 'red', True, True )
+
+    def draw_airfoil_points(self, airfoil, xpos, ypos ):
+        self.draw_airfoil(airfoil, xpos, ypos, '1px', 'red', False, True )
+
+    def save(self):
+        self.dwg.save()
