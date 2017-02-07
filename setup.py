7a67d3ec91f8a7f08737ce16a8e1d2ae83ba3071
Curtis L. Olson <curtolson@flightgear.org>
2017-02-07 13:36:02 -0600

    Officially license with GPLv3.

--- setup.py
+++ setup.py
@@ -10,9 +10,11 @@ setup( name = 'madesigner',
        author = 'Curtis L. Olson',
        author_email = 'curtolson@flightgear.org',
        url = 'http://madesigner.flightgear.org',
+       license = "GPLv3",
        package_dir = {'': 'madesigner'},
        packages = find_packages('madesigner'),
        scripts = [ 'madesigner/madesigner.py' ],
        package_data = { 'madlib': ['airfoils/*.dat'] },
+       install_requires = ['svgwrite', 'Polygon2'],
        )
 
